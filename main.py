79f2883985a9c9f99e0b3d3641474c8e0d98856c
Qianni Zhang <>
2021-05-17 20:32:11 +0000

    Method 1 is wrong

--- main.py
+++ main.py
@@ -6,15 +6,8 @@ print(student_scores)
 # 🚨 Don't change the code above 👆
 
 #Write your code below this row 👇
-# for max in range (0, len (student_scores)-1):
-
-#   if student_scores[max] > student_scores[max+1]:
-#      Max_score = student_scores[max]
 
 
-# print (f"The highest score in the class is: {Max_score}")
-
-# method 2:
 Max_score = 0
 for Max in student_scores:
   if Max > Max_score:
